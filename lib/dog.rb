f65f1cbe5e19e6f845955bc2998b2896e748320a
bcharlotin1 <biancacharlotin1@gmail.com>
2021-02-24 21:08:09 -0500

    commit tree 9445f79cb583f60f4ef839ba324afe9267901e29

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,24 @@
-class Dog
+ require 'pry'
+class Dog < ActiveRecord::Base
+    
+    # attr_accessor :name, :breed
+    # # attr_reader :id
+ 
+
+    # def update(hash)
+    #     dog = Dog.find(1)
+    #     # dog = self
+    #     dog.name = hash[:name]
+    #     # dog.id = self.id
+    #     dog.save
+    #     # dog
+    #     # binding.pry
+
+    # end
+    
+    # # def self.find_or_create_by
+        
+    # # end
+  
+
 end
